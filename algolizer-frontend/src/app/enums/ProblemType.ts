00d19cc141ebb12afef715a37302f3f0959fa46d
Armin Petrović <apetrovic1@etf.unsa.ba>
2023-02-15 17:06:14 +0100

    Added types of problems as enum

---
+++ algolizer-frontend/src/app/enums/ProblemType.ts
@@ -0,0 +1,17 @@
+enum ProblemType {
+    MERGE_SORT = "Merge sort algorithm",
+    QUICK_SORT = "Quick sort algorithm",
+    BUBBLE_SORT = "Bubble sort algorithm",
+    BINARY_SEARCH = "Binary search on sorted array",
+    LINKED_LIST = "Linked list operations",
+    QUEUE = "Queue algorithms analysis", //BFS
+    STACK = "Stack algorithms analysis", //DFS
+    BST = "Binary search tree algorithms",
+    EDMONDS_KARP = "Edmonds karp maximum flow algorithm",
+    DIJKSTRA = "Dijkstra shortest path finder",
+    KRUSKAL = "Kruskal minimum spanning tree",
+    HEAP = "Heap structure algorithms",
+    HASH_MAP = "Hashing structures and algorithms"
+};
+
+export default ProblemType;
